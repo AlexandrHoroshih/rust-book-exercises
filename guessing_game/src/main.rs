13aa0635862de33c99f3e6fd9f2715c9bf6a10fc
AlexandrHoroshih <xad71rus@gmail.com>
2021-05-21 23:47:16 +0300

    feat(guessing_game): handle all cases

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
@@ -12,8 +13,15 @@ fn main() {
 
     io::stdin()
         .read_line(&mut guess)
-        .expect("Could not read the line!");
+        .expect("Could not read the line! (°ロ°)☝");
 
-    println!("Your guess: {}", guess);
+    println!("Your guess ( ͡° ͜ʖ ͡°) {}", guess);
 
+    let guess: u32 = guess.trim().parse().expect("Please type a number! (°ロ°)☝");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small ¯\\_(ツ)_/¯"),
+        Ordering::Greater => println!("Too big ( ͡° ͜ʖ ͡°)"),
+        Ordering::Equal => println!("(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ You win! ✧ﾟ･: *ヽ(◕ヮ◕ヽ)")
+    }
 }
