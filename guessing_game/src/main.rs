032202e3aa841357971e8e031e4e551155b106e6
AlexandrHoroshih <xad71rus@gmail.com>
2021-05-21 23:53:24 +0300

    feat(guessing_game): add game loop

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -7,21 +7,33 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..101);
 
-    println!("Input your guess! ☜(˚▽˚)☞");
 
-    let mut guess = String::new();
+    loop {
+        println!("Input your guess! ☜(˚▽˚)☞");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Could not read the line! (°ロ°)☝");
-
-    println!("Your guess ( ͡° ͜ʖ ͡°) {}", guess);
-
-    let guess: u32 = guess.trim().parse().expect("Please type a number! (°ロ°)☝");
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small ¯\\_(ツ)_/¯"),
-        Ordering::Greater => println!("Too big ( ͡° ͜ʖ ͡°)"),
-        Ordering::Equal => println!("(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ You win! ✧ﾟ･: *ヽ(◕ヮ◕ヽ)")
+        let mut guess = String::new();
+    
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Could not read the line! (°ロ°)☝");
+    
+        println!("Your guess ( ͡° ͜ʖ ͡°) {}", guess);
+    
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please type a number! (°ロ°)☝");
+                continue;
+            }
+        };
+    
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small ¯\\_(ツ)_/¯"),
+            Ordering::Greater => println!("Too big ( ͡° ͜ʖ ͡°)"),
+            Ordering::Equal => {
+                println!("(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ You win! ✧ﾟ･: *ヽ(◕ヮ◕ヽ)");
+                break;
+            }
+        }
     }
 }
