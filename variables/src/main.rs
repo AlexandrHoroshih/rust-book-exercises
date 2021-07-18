aa56a3711c3e9d9171574e57cf2ac07b59fb2d86
AlexandrHoroshih <xad71rus@gmail.com>
2021-07-18 21:48:24 +0300

    feat(variables): add both shadowing and mutable vars

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,12 @@
 fn main() {
+    // shadowing
     let x = 5;
     println!("The value of x is: {}", x);
     let x = 6;
     println!("The value of x is: {}", x);
+    // mutable
+    let mut y = 7;
+    println!("The value of y is: {}", y);
+    y = 8;
+    println!("The value of y is: {}", y);
 }
