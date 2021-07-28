3d9ed8ae1b646e155605e3476150d7a0967fb83d
AlexandrHoroshih <xad71rus@gmail.com>
2021-07-29 01:06:16 +0700

    feat(funcs): add funcs stuff

--- funcs/src/main.rs
+++ funcs/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    my_custom_func("Hello from custom func!", plus_one(6));
+}
+
+fn my_custom_func(somestring: &str, number: i32) {
+    println!("My string is \"{}\", my number is {}", somestring, number);
+}
+
+fn plus_one(x: i32) -> i32 {
+    // last expression in func is returns
+    // if we add ; at the end - then it will be statement that does not return value
+    x + 1
 }
